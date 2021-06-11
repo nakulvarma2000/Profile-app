15a3e61155331cff8268e8a61d9c1cc697421d19
nakulvarma2000 <nakulvarma2000@gmail.com>
2021-06-11 15:12:52 +0530

    metadata-4

--- profile-app/src/components/MainContent/AboutMe/AboutMe.js
+++ profile-app/src/components/MainContent/AboutMe/AboutMe.js
@@ -10,7 +10,7 @@ export default class AboutMe extends Component {
             <div>
                 <div className="aboutme-card-page" >
                     <Card className='aboutme-card '>
-                        <Card.Img variant='top' alt="Nakul's image"  src={'/images/Profilepic.JPG'} style={{display:'block',width:'25%',marginLeft:'auto',marginRight:'auto',marginTop:'0.5rem',borderRadius:'50%',boxShadow: ' 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }} />
+                        <Card.Img variant='top' alt="Nakul's image"  src={'/images/Profilepic.jpeg'} style={{display:'block',width:'25%',marginLeft:'auto',marginRight:'auto',marginTop:'0.5rem',borderRadius:'50%',boxShadow: ' 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }} />
                         <Card.Body>
                             <Card.Title className='align-centre name'>{profileData.name} </Card.Title>
                             <Card.Body style={{padding: "0.50rem" }}>
